1421ac904c894e85f504cac1aaaa77b4c93e817e
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-09-26 18:31:53 -0400

    completed navbar

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "My home page" });
+  res.render("index", { title: "Home" });
+});
+
+/* GET about me page. */
+router.get("/about", function (req, res, next) {
+  res.render("index", { title: "About" });
 });
 
 /* GET projects page. */
 router.get("/projects", function (req, res, next) {
-  res.render("index", { title: "projectsss" });
+  res.render("index", { title: "Projects" });
 });
 
 /* GET services page. */
 router.get("/services", function (req, res, next) {
-  res.render("index", { title: "Servicess" });
+  res.render("index", { title: "Services" });
 });
 
 /* GET home page. */
 router.get("/contact", function (req, res, next) {
-  res.render("index", { title: "contact me pagee" });
+  res.render("index", { title: "Contact Me" });
 });
 
 module.exports = router;
