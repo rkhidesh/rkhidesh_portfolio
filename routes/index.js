0e0eda308e4746a1305e5f3a308dbf48fc1ddb0b
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-09-26 14:51:42 -0400

    created routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "My home page" });
+});
+
+/* GET projects page. */
+router.get("/projects", function (req, res, next) {
+  res.render("index", { title: "projectsss" });
+});
+
+/* GET services page. */
+router.get("/services", function (req, res, next) {
+  res.render("index", { title: "Servicess" });
+});
+
+/* GET home page. */
+router.get("/contact", function (req, res, next) {
+  res.render("index", { title: "contact me pagee" });
 });
 
 module.exports = router;
